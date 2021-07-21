e0e2933ec680302bb3ee7d4d0c03033032c02280
Cristian Campos <camposcristian90@gmail.com>
2021-07-22 00:30:34 +1000

    index from components

---
+++ components/index.ts
@@ -0,0 +1,8 @@
+export { default as Header } from './Header/Header';
+export { default as Footer } from './Footer/Footer';
+
+export { default as Button } from './Button/Button';
+export { default as Card } from './Card/Card';
+export { default as ScrollToTop } from './ScrollToTop/ScrollToTop';
+export { default as SearchBar } from './SearchBar/SearchBar';
+export { default as Toast } from './Toast/Toast';
